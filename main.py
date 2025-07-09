a6e4125dcc30a82789c05a7d70be71bf8681ff4c
malikissad <malikisad24@gmail.com>
2025-07-09 16:18:23 +0100

    pre-processing

---
+++ main.py
@@ -0,0 +1,3 @@
+from src.preprocessing import preprocessing
+if __name__ == "__main__":
+  preprocessing("data/datasets/Lung cancer segmentation dataset with Lung-RADS class/Lung cancer segmentation dataset with Lung-RADS class/lung_cancer_train.pkl")
---
+++ src/preprocessing.py
@@ -0,0 +1,50 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import tensorflow as tf 
+from sklearn.model_selection import train_test_split
+
+def preprocessing(chemin):
+
+# chargement des données
+  data = pd.read_pickle(chemin)
+
+  print(type(data))
+  print(data.shape)
+
+
+#masque et image
+  masks = data['mask']
+  images_net = data['hu_array']
+
+#transformer en tableau NumPy + canal
+
+  X = np.stack(images_net.values)[...,np.newaxis]
+  Y = np.stack(masks.values)[...,np.newaxis]
+
+  print(X.shape)
+  print(Y.shape)
+
+#resize
+  X_resize = tf.image.resize(X,[256,256]).numpy()
+  Y_resize = tf.image.resize(Y,[256,256],method='nearest').numpy()
+
+#canal 3 recommender pour swin-unit
+  X_canal = np.repeat(X_resize,3,axis=-1)
+
+# Normaliser uniquement les images
+  X_norm = (X_canal - X_canal.min()) / (X_canal.max() - X_canal.min())
+
+# S'assurer que les masques sont bien binaires
+  Y_bin = (Y_resize > 0).astype(np.uint8)
+
+  print(X_norm)
+  print(Y_bin)
+
+
+  X_train, X_test, y_train, y_test = train_test_split(X_norm, Y_bin, test_size=0.2, random_state=42)
+
+  np.save("data/pre-procecing/X_train.npy",X_train)
+  np.save("data/pre-procecing/X_test.npy",X_test)
+  np.save("data/pre-procecing/y_train.npy",y_train)
+  np.save("data/pre-procecing/y_test.npy",y_test)
